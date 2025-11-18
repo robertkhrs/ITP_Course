14aec681b61a0a1f19966330e12bc48721653362
Robert Kharisov <robertkharisov@MacBook-Air-Robert.local>
2025-11-18 17:59:28 +0300

    Lab 11: Exercise 2 files and solution added

---
+++ src/Lab_11/Exercise_2.java
@@ -0,0 +1,24 @@
+package Lab_11;
+import java.io.*;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        try (FileInputStream input = new FileInputStream("/Users/robertkharisov/IdeaProjects/Lab Assignments/src/Lab_11/input.txt");
+             FileOutputStream output = new FileOutputStream("/Users/robertkharisov/IdeaProjects/Lab Assignments/src/Lab_11/output.txt")) {
+            int data;
+
+            while ((data = input.read()) != -1) {
+                output.write(data);
+            }
+
+            System.out.println("File was successfully written");
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File access error: File not found or no write permission.");
+            System.err.println("Details: " + e.getMessage());
+
+        } catch (IOException e) {
+            System.out.println("Input/output error: " + e.getMessage());
+        }
+    }
+}
